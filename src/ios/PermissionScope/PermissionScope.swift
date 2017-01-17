9b30274959bd2530233eaa4a7b8ffb9d94a8f28a
Ignacio González Bullón <ignacio.gonzalez@hp.com>
2017-01-17 18:00:37 +0100

    Fix guard let with sequence expression

--- src/ios/PermissionScope/PermissionScope.swift
+++ src/ios/PermissionScope/PermissionScope.swift
@@ -618,7 +618,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
         DispatchQueue.main.asyncAfter(
             deadline: DispatchTime.now() + Double(Int64(0.1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: {
                 self.getResultsForConfig { results in
-                  if let notificationResult = results!.first ({ $0.type == .notifications }) {
+                  if let notificationResult = results.first(where: { $0.type == .notifications }) {
                     if notificationResult.status == .unknown {
                       self.showDeniedAlert(notificationResult.type)
                     } else {
